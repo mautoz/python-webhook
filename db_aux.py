c57b0cc672cb83beba466d8374bdce69afac9aae
Mauricio Tavares Ozaki <ozaki@versatushpc.com.br>
2020-11-22 01:44:21 -0300

    Adicionando últimos elementos.

--- db_aux.py
+++ db_aux.py
@@ -1,5 +1,11 @@
+from datetime import date
 import psycopg2 as db
+from random import randrange
+import random as rand
+import datetime
 from contextlib import contextmanager
+
+rand.seed(datetime.datetime.now())
 
 @contextmanager
 def connect_db(db_credentials):
@@ -37,4 +43,26 @@ def insert_db(conn, table, review):
 def search_file(conn, table, id):
     cur = conn.cursor()
     cur.execute(f"SELECT review_app, review_raw FROM {table} WHERE id={id}")
-    return cur.fetchone()
+    current_review = cur.fetchone()
+    return current_review
+
+def random_review(conn, table):
+    cur = conn.cursor()
+    nrows = total_rows(conn, table)
+    random_id = randrange(nrows)
+
+    cur.execute(f"SELECT id, review_app, review_raw FROM {table} WHERE id={random_id}")
+    current_review = cur.fetchone()
+
+    while current_review == None:
+        random_id = randrange(nrows)
+        cur.execute(f"SELECT id, review_app, review_raw FROM {table} WHERE id={random_id}")
+        current_review = cur.fetchone()
+    
+    return current_review
+
+def total_rows(conn, table):
+    cur = conn.cursor()
+    cur.execute(f"SELECT count(*) FROM {table} WHERE is_a11y_human=-1")
+    result = cur.fetchone()
+    return result[0]
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, session, flash, url_for, send_from_directory
 from flask.templating import render_template
 import os
+
 import db_aux
 
 db_credentials = {
@@ -28,8 +29,16 @@ def imagem(nome_arquivo):
 @app.route('/classificar')
 def classificar():
     with db_aux.connect_db(db_credentials) as conn:
-        review = db_aux.search_file(conn, "reviews_data", 8)
-    return render_template('classificar.html', titulo = "Classificar Review", review=review)
+        review = db_aux.random_review(conn, "reviews_data")
+        total = db_aux.total_rows(conn, "reviews_data")
+    return render_template('classificar.html', titulo = "Classificar Review", review=review, total=total)
+
+@app.route('/atualizar')
+def atualizar():
+    with db_aux.connect_db(db_credentials) as conn:
+        review = db_aux.random_review(conn, "reviews_data")
+        total = db_aux.total_rows(conn, "reviews_data")
+    return render_template('classificar.html', titulo = "Classificar Review", review=review, total=total)
 
 if __name__ == "__main__":
     app.run(debug=True)
