df97afc60b9365354013eaa19985db22d53659cc
Mauricio Tavares Ozaki <ozaki@versatushpc.com.br>
2020-12-01 11:08:17 -0300

    Deixando o run para ser configurado pelo user.

--- run.sh
+++ run.sh
@@ -5,12 +5,14 @@ export FLASK_APP=main.py
 export FLASK_ENV=development
 
 # Altere de acordo com o seu postgres
+
+# Padrão costuma ser: export POSTGRES_HOST=localhost
 export POSTGRES_HOST=
-# Se seguiu as instruções do review-classifier o database será:
-# export POSTGRES_DATABASE=reviews-classifier
+# O padrão aqui é: export POSTGRES_DATABASE=reviews-classifier
 export POSTGRES_DATABASE=
 export POSTGRES_USER=
 export POSTGRES_PASSWORD=
+# Padrão costuma ser: export POSTGRES_PORT=5432
 export POSTGRES_PORT=
 
 python3 -m flask run 
